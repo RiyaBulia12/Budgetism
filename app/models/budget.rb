d86059a94136a24eecea58e3ea6235ef1f4e79d6
riyabulia12 <riyabulia12@gmail.com>
2023-01-03 23:53:37 +0530

    Update models with relationships

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,2 +1,7 @@
 class Budget < ApplicationRecord
+  belongs_to :user
+  has_many :category_budgets, foreign_key: :budget_id, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: true
 end
