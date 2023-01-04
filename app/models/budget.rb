2d4b28afe689a55e837530ae83c368271e9a240f
riyabulia12 <riyabulia12@gmail.com>
2023-01-04 17:40:54 +0530

    Update amount validation in budget class

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -3,5 +3,5 @@ class Budget < ApplicationRecord
   has_many :category_budgets, foreign_key: :budget_id, dependent: :destroy
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
